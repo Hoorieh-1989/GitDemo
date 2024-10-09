7924fbcd1d92ff3cfd0dc849069fea04d86bee46
hoorieh.23 <hoorieh.23@gmail.com>
2024-10-09 12:01:30 +0200

    Add project files.

---
+++ GitDemo/Form1.cs
@@ -0,0 +1,11 @@
+namespace GitDemo
+{
+    //changes for commit 
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+    }
+}
